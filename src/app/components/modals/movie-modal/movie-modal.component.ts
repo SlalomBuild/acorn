1390ed5fa3e53c925ff6f4ec7cbc1d9466799270
Paul Stahmer <paul.stahmer@slalom.com>
2019-05-06 12:40:04 -0500

    Finished implementing modals/movie-modal

--- src/app/components/modals/movie-modal/movie-modal.component.ts
+++ src/app/components/modals/movie-modal/movie-modal.component.ts
@@ -1,13 +1,32 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Movie } from 'app/models';
 
 @Component({
   selector: 'movie-modal',
   templateUrl: './movie-modal.component.html',
-  styleUrls: ['./movie-modal.component.css']
+  styleUrls: ['./movie-modal.component.scss']
 })
 export class MovieModalComponent {
   @Input() movie: Movie;
   @Output() onUpdate = new EventEmitter<Movie>();
+
+  movieForm: FormGroup;
+
+  constructor(public fb: FormBuilder) {
+    this.movieForm = fb.group({
+      id: '',
+      title: ['', [ Validators.required ]],
+      description: ['', [ Validators.required ]]
+    });
+  }
+
+  ngOnChanges() {
+    this.movieForm.patchValue(this.movie);
+  }
+
+  updateMovie() {
+    this.onUpdate.emit(this.movieForm.value);
+  }
 }
